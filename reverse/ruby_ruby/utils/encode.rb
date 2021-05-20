2a58f5aa1b0c45e3269e4e485ee338c54bc00aed
Kowalski Dark <darkkowalski2012@gmail.com>
2021-05-20 20:19:45 +0800

    Add ruby ruby flag

--- reverse/ruby_ruby/utils/encode.rb
+++ reverse/ruby_ruby/utils/encode.rb
@@ -1,25 +1,39 @@
 #!/usr/bin/env ruby
 
 class Compiler
+  def self.jamming
+    source = <<~GETTYSBURG
+      Four score and seven years ago our fathers brought forth on this continent,
+      a new nation, conceived in Liberty, and dedicated to the proposition that
+      all men are created equal.
+    GETTYSBURG
+
+    source.unpack('B*')
+  end
+
   def self.template(flag: '', dummy: '')
     flag = flag.unpack('B*')
     dummy = dummy.unpack('B*')
-
+    jamming = self.jamming
     result = <<~TEMPLATE
       class Flag
         def self.flag
+          $stderr.puts #{jamming}
           #{dummy}
         end
 
         def self.to_s
+          $stderr.puts #{jamming}
           self.flag.pack('B*')
         end
 
         def self.dummy
+          $stderr.puts #{jamming}
           #{flag}
         end
 
         def self.to_a
+          $stderr.puts #{jamming}
           [self.dummy.pack('B*')]
         end
 
@@ -38,4 +52,4 @@ class Compiler
 end
 
 # Encode!
-Compiler.compile(flag: "flag_{real}", dummy: "flag_{dummy}") #=> flag.rb
+Compiler.compile(flag: "tjctf{R0smOnt1s}", dummy: "tjctf{YukihiroMatsumoto}") #=> flag.rb
