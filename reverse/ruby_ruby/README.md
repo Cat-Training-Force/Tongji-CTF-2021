d092fc2e051dfbb4fef75844858e6abfb5045de2
Kowalski Dark <darkkowalski2012@gmail.com>
2021-05-20 19:34:36 +0800

    Ruby Ruby!

---
+++ reverse/ruby_ruby/README.md
@@ -0,0 +1,7 @@
+# Ruby Ruby
+
+For Ruby 3
+
+Read `encode.rb` and change the last line.
+
+Run `ruby encode.rb` to generate `flag.rb`
---
+++ reverse/ruby_ruby/encode.rb
@@ -0,0 +1,39 @@
+class Compiler
+  def self.template(flag: '', dummy: '')
+    flag = flag.unpack('B*')
+    dummy = dummy.unpack('B*')
+
+    result = <<~TEMPLATE
+      class Flag
+        def self.flag
+          #{dummy}
+        end
+
+        def self.to_s
+          self.flag.pack('B*')
+        end
+
+        def self.dummy
+          #{flag}
+        end
+
+        def self.to_a
+          [self.dummy.pack('B*')]
+        end
+
+        def self.hint
+          'RubyVM::InstructionSequence'
+        end
+      end
+    TEMPLATE
+  end
+
+  def self.compile(flag: '', dummy: '')
+    bytecode = RubyVM::InstructionSequence.compile(self.template(flag: flag, dummy: dummy))
+
+    File.write( 'flag.rb', bytecode.to_binary)
+  end
+end
+
+# Encode!
+Compiler.compile(flag: "flag_{real}", dummy: "flag_{dummy}") #=> flag.rb
